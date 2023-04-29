72aefe780ea7dd21d5c2dbec2f9ec56cd1956000
Artaxerxes Nazareno <artaxerxesnazare@hotmail.com>
2023-04-29 07:39:26 +0100

    rename table user to users

--- src/app/models/UserDAO.php
+++ src/app/models/UserDAO.php
@@ -20,14 +20,14 @@ class UserDAO
       $password = mysqli_real_escape_string($conn,$user->getPassword());
 
 //      $hashedPassword = $this->getHashedPassword($password);
-      $query = "INSERT INTO user (name, email, password) VALUES ('$name', '$email', '$password')";
+        $query = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
 
       $result = mysqli_query($conn, $query);
   }
 
   public static function getUserByEmailAndPassword($email, $password){
         $conn = Conexao::getInstance();
-        $query = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
+        $query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
         $result = mysqli_query($conn, $query);
         $user = mysqli_fetch_assoc($result);
         return $user;
