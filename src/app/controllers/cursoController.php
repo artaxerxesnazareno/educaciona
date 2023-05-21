2ebde2958a9ea8e09d0c178eba6625addd7caa77
Artaxerxes Nazareno <artaxerxesnazare@hotmail.com>
2023-05-21 19:55:34 +0100

    cadastrando e deletando curso

--- src/app/controllers/cursoController.php
+++ src/app/controllers/cursoController.php
@@ -6,12 +6,83 @@ require '../../../autoloader.php';
 
 $curso = CursoDAO::getCursosAllAula(1);
 session_start();
-$_SESSION['curso'] = $curso;
-//echo json_encode($curso);
-//echo var_dump($curso);
 
-$c =$_SESSION['curso'];
+function completarAula($idAula, $idCurso){
+CursoDAO::completarAula($idAula);
+header('Location: ../../resources/views/curso.php?id='.$idCurso.'&aula='.($idAula +1));
+}
+function cadastrarCurso(){
+    try {
+        if (isset($_FILES['capaCurso']) && $_FILES['capaCurso']['error'] == 0) {
+            $allowed_extensions = array('png', 'jpeg', 'jpg');
+            $file_info = pathinfo($_FILES['capaCurso']['name']);
+            $file_extension = strtolower($file_info['extension']);
+
+            if (in_array($file_extension, $allowed_extensions)) {
+                $upload_directory = '../../resources/assets/images/capas/';
+                $new_file_name = uniqid() . '.' . $file_extension;
+
+                if (move_uploaded_file($_FILES['capaCurso']['tmp_name'], $upload_directory . $new_file_name)) {
+                    $nome_curso = $_POST['nomeCurso'];
+                    $nivel_curso = $_POST['nivelCurso'];
+                    $categoria_curso = $_POST['categoriaCurso'];
+                    $descricao_curso = $_POST['descricaoCurso'];
+
+                    // Armazene os dados do formulário no banco de dados ou em outro local conforme necessário.
+                    $curso = new Curso($nome_curso, $descricao_curso);
+                    $curso->setNivel($nivel_curso);
+                    $curso->setCapa($new_file_name);
+                    CursoDAO::saveCurso($curso);
+
+
+//                    header("Location: ../../resources/views/dashboard/resultado.html?result=success");
+                    header("Location: ../../resources/views/dashboard/index.php");
+                    exit();
+                } else {
+                    throw new Exception('Erro ao mover o arquivo para o diretório destino.');
+                }
+            } else {
+                throw new Exception('Tipo de arquivo não permitido.');
+            }
+        } else {
+            throw new Exception('Nenhum arquivo enviado ou erro no envio.');
+        }
+    } catch (Exception $e) {
+        header("Location: ../../resources/views/dashboard/resultado.html?result=error&message=" . urlencode($e->getMessage()));
+        exit();
+    }
+}
+function deletarCurso() {
+    try {
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $result = CursoDAO::deleteCurso($id);
+
+            if ($result) {
+                header("Location: ../../resources/views/dashboard/index.php?result=success");
+                exit();
+            } else {
+                throw new Exception("Erro ao excluir o curso.");
+            }
+        } else {
+            throw new Exception("ID do curso não fornecido.");
+        }
+    } catch (Exception $e) {
+        header("Location: ../../resources/views/dashboard/resultado.html?result=error&message=" . urlencode($e->getMessage()));
+        exit();
+    }
+}
 
 
 
-var_dump($c);
+if (isset($_GET['aula'])) {
+    completarAula($_GET['aula'], $_GET['id']);
+}
+if (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['cadastrarCurso'])) {
+    cadastrarCurso();
+}
+if (($_SERVER['REQUEST_METHOD'] == 'GET') && isset($_GET['deletarCurso'])) {
+    deletarCurso();
+}
+
+?>
--- src/app/models/CursoDAO.php
+++ src/app/models/CursoDAO.php
@@ -85,4 +85,112 @@ static public  function getCurso($id)
 
         return $row['total_inscritos'];
     }
+
+    public static function completarAula($id)
+    {
+        $conn = Conexao::getInstance();
+        $sql = "UPDATE aulas SET completada = 1 WHERE id = $id";
+        $result = mysqli_query($conn, $sql);
+        self::getCursosAll();
+    }
+
+    static public function progressoCurso($id, $id_user)
+    {
+        $conn = Conexao::getInstance();
+        $sql1 = "select count(id) as total_aulas from aulas where curso_id ='$id'";
+        $result1 = mysqli_query($conn, $sql1);
+        $row1 = $result1->fetch_assoc();
+        $total_aulas = $row1['total_aulas'];
+
+        $sql2 = "select count(id) as completadas from aulas where curso_id ='$id' and completada = 1";
+//        $sql2 = "select count(ac.id) as total_aulas from aulas_concluidas ac join aulas a on a.id = ac.aula_id join cursos c on c.id = a.curso_id where c.id = '$id_user'";
+        $result2 = mysqli_query($conn, $sql2);
+        $row2 = $result2->fetch_assoc();
+        $completadas = $row2['completadas'];
+
+        $progresso = ($completadas / $total_aulas) * 100;
+        return $progresso;
+    }
+
+    public static function getTopCursos()
+    {
+        $cursoTop = [];
+        $conn = Conexao::getInstance();
+        $sql = "select cursos.id, count(inscritos.curso_id) as total_inscritos from inscritos join cursos on cursos.id = inscritos.curso_id  group by cursos.id order by total_inscritos desc limit 5";
+        $result = mysqli_query($conn, $sql);
+        foreach ($result as $row) {
+            $curso = self::getCurso($row['id']);
+            $cursoTop[] = $curso;
+        }
+        return $cursoTop;
+    }
+
+    public static function getCursosByCategoria($categoria_id)
+    {
+        $conn = Conexao::getInstance();
+        $sql = "select cursos.id from cursos  where cursos.categoria_id = '$categoria_id'";
+        $result = mysqli_query($conn, $sql);
+        $cursos = [];
+        foreach ($result as $row) {
+            $curso = self::getCurso($row['id']);
+        }
+        return $cursos;
+    }
+
+    public static function getCursosByUserId($user_id)
+    {
+        $conn = Conexao::getInstance();
+        $sql = "SELECT t.curso_id FROM inscritos t join users on t.user_id = users.id WHERE users.id = '$user_id'";
+        $result = mysqli_query($conn, $sql);
+        $cursos = [];
+        foreach ($result as $row) {
+            $curso = self::getCurso($row['curso_id']);
+            $cursos[] = $curso;
+        }
+        return $cursos;
+    }
+
+    public static function saveCurso(Curso $curso){
+        $conn = Conexao::getInstance();
+
+        $name = mysqli_real_escape_string($conn, $curso->getNome());
+        $descricao = mysqli_real_escape_string($conn, $curso->getDescricao());
+        $capa = mysqli_real_escape_string($conn, $curso->getCapa());
+        $nivel = mysqli_real_escape_string($conn, $curso->getNivel());
+        $categoriaID = mysqli_real_escape_string($conn, $curso->getCategoriaId());
+
+
+
+        $sql = " INSERT INTO educacionaDB.cursos (name, descricao, imagen_capa, nivel, categoria_id) VALUES ('$name', '$descricao', ' $capa', '$nivel', 1)";
+        $result = mysqli_query($conn, $sql);
+    }
+    public static function deleteCurso($id)
+    {
+        $conn = Conexao::getInstance();
+        $id = mysqli_real_escape_string($conn, $id);
+
+        // Deletar todas as aulas relacionadas ao curso
+        $sqlAulas = "DELETE FROM aulas WHERE curso_id = '$id'";
+        mysqli_query($conn, $sqlAulas);
+
+        // Deletar o curso
+        $sqlCurso = "DELETE FROM cursos WHERE id = '$id'";
+        $result = mysqli_query($conn, $sqlCurso);
+
+        // Retorna true se o curso foi excluído com sucesso, caso contrário, retorna false
+        return $result;
+    }
+    public static function saveAula(Aula $aula, $curso_id)
+    {
+        $conn = Conexao::getInstance();
+
+        $nome = mysqli_real_escape_string($conn, $aula->getNome());
+        $link = mysqli_real_escape_string($conn, $aula->getLink());
+        $descricao = mysqli_real_escape_string($conn, $aula->getDescricao());
+        $completada = mysqli_real_escape_string($conn, $aula->getCompletada());
+
+        $sql = "INSERT INTO aulas (nome, link, descricao, completada, curso_id) VALUES ('$nome', '$link', '$descricao', '$completada', '$curso_id')";
+        $result = mysqli_query($conn, $sql);
+    }
+
 }
